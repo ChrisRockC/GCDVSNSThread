91d784df32226402589db0a60144af9684731d27
chenchaocc <ccerdong@sina.cn>
2015-06-17 01:53:09 +0800

    GCD对比NSThread
    GCD对比NSThread

--- GCDVSNSThread/ViewController.m
+++ GCDVSNSThread/ViewController.m
@@ -14,14 +14,21 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self gcdDemo];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)gcdDemo{
+    //这些方法内存实现是封装了，我们是通过相关提示如：completion 知其调用时期
+    //或者我们可以NSLog调试也可知道其中的调用顺序
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        NSLog(@"异步操作任务--%@",[NSThread currentThread]);
+        
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            NSLog(@"回到主线程更新UI----%@",[NSThread currentThread]);
+        });
+        
+    });
 }
 
 @end
